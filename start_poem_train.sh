f9599bcca09104bc30b3cb28602f048f916520b3
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-10 09:32:10 +0800

    modified

--- start_poem_train.sh
+++ start_poem_train.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
 # multiple command in one line
-
-
 if [ -f ".trainpid" ]; then
     if [ -d /proc/`cat .trainpid` ]; then
         echo found running pid `cat .trainpid`
     else
         rm .trainpid \
         && echo [remove dead pid `cat .trainpid`] \
-        && nohup python3 poem.py >logs/train.out 2>&1 & echo $! > .trainpid \
+        && nohup python3 train.py >logs/train.out 2>&1 & echo $! > .trainpid \
         && echo [train started] \
         && busybox tail -f logs/train.out
     fi
 else
-    nohup python3 poem.py >logs/train.out 2>&1 & echo $! > .trainpid \
+    nohup python3 train.py >logs/train.out 2>&1 & echo $! > .trainpid \
     && echo [train started] \
     && busybox tail -f logs/train.out
 fi
