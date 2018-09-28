7bce5cd8801acd49a6a36ea2e88937cbe9f04297
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-28 14:34:01 +0800

    modified

--- poem_begin_train.sh
+++ poem_begin_train.sh
@@ -7,12 +7,12 @@ if [ -f ".trainpid" ]; then
     else
         rm .trainpid \
         && echo [remove dead pid `cat .trainpid`] \
-        && nohup python3 train.py --learning_rate=0.001 --epochs=1000 --input_name=poems >logs/train.out 2>&1 & echo $! > .trainpid \
+        && nohup python3 train.py --cuda_visible_devices=2 --learning_rate=0.001 --epochs=1000 --input_name=poems >logs/train.out 2>&1 & echo $! > .trainpid \
         && echo [train started] \
         && busybox tail -f logs/train.out
     fi
 else
-    nohup python3 train.py --learning_rate=0.001 --epochs=1000 --input_name=poems >logs/train.out 2>&1 & echo $! > .trainpid \
+    nohup python3 train.py --cuda_visible_devices=2 --learning_rate=0.001 --epochs=1000 --input_name=poems >logs/train.out 2>&1 & echo $! > .trainpid \
     && echo [train started] \
     && busybox tail -f logs/train.out
 fi
--- poem_gen.sh
+++ poem_gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python3 compose_poem.py --input_name=poems
+python3 compose_poem.py --input_name=poems --cuda_visible_devices=3
 
 
