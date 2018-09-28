3410cbedb036d132c25761413620d27dc8cff52f
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-28 13:31:57 +0800

    modified

--- tensorboard.sh
+++ tensorboard.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 if [ -f ".tensorboardpid" ]; then
     if [ -d /proc/`cat .tensorboardpid` ]; then
-        echo found running tensorboard pid `cat .tensorboardpid`
+        echo [kill live pid `cat .tensorboardpid`] \
+        && kill `cat .tensorboardpid`
+
     else
         echo [remove dead pid `cat .tensorboard`] \
-        && rm .tensorboard \
-        && nohup tensorboard --port 10087 --logdir=logs/ >logs/tensorboard.out 2>&1 & echo $! > .tensorboardpid \
-        && echo [tensorboard started] \
-        && busybox tail -f logs/tensorboard.out
+        && rm .tensorboard
     fi
-else
-    nohup tensorboard --port 10087 --logdir=logs/ >logs/tensorboard.out 2>&1 & echo $! > .tensorboardpid \
-    && echo [tensorboard started] \
-    && busybox tail -f logs/train.out
 fi
+
+nohup tensorboard --port 10086 --logdir=logs/ >logs/tensorboard.out 2>&1 & echo $! > .tensorboardpid \
+    && echo [start tensorboard on port 10086]
