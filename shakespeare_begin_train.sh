c386ea723cfb9f1ee6b5ff50ebeeccc0b9247ecf
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-28 18:40:30 +0800

    modified

--- shakespeare_begin_train.sh
+++ shakespeare_begin_train.sh
@@ -7,12 +7,12 @@ if [ -f ".sptrainpid" ]; then
     else
         rm .sptrainpid \
         && echo [remove dead pid `cat .sptrainpid`] \
-        && nohup python3 train.py --cuda_visible_devices=1 --input_name=shakespeare --learning_rate=0.001 --epochs=1000 --input_name=poems >logs/train.out 2>&1 & echo $! > .sptrainpid \
+        && nohup python3 train.py --cuda_visible_devices=1 --input_name=shakespeare --learning_rate=0.001 --epochs=1000 --input_name=poems >logs/sptrain.out 2>&1 & echo $! > .sptrainpid \
         && echo [train started] \
         && busybox tail -f logs/sptrain.out
     fi
 else
-    nohup python3 train.py --cuda_visible_devices=1 --input_name=shakespeare --learning_rate=0.001 --epochs=1000 --input_name=poems >logs/train.out 2>&1 & echo $! > .sptrainpid \
+    nohup python3 train.py --cuda_visible_devices=1 --input_name=shakespeare --learning_rate=0.001 --epochs=1000 --input_name=poems >logs/sptrain.out 2>&1 & echo $! > .sptrainpid \
     && echo [train started] \
     && busybox tail -f logs/sptrain.out
 fi
