2d8e278a902c51cabcf6445059ee56574f3c97b4
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-29 11:29:20 +0800

    modified

--- poem_gen.sh
+++ poem_gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python3 rnn.py --mode=compose --input_name=poems --cuda_visible_devices=3
+python3 rnn.py --mode=compose --input_name=poems --rnn_size=256 --num_layers=3 --cuda_visible_devices=3
 
 
