c44dd8869839514913ff3bed9ec17e3cb39250ef
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-29 11:24:44 +0800

    modified

--- shakespeare_gen.sh
+++ shakespeare_gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python3 rnn.py --mode=compose --input_name=shakespeare --gen_sequence_len=2000 --cuda_visible_devices=3
+python3 rnn.py --mode=compose --input_name=shakespeare --rnn_size=256 --num_layers=3 --gen_sequence_len=2000 --cuda_visible_devices=3
 
 
