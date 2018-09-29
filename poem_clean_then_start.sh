b5d8603ae7bf41418904196ee9e2745c091cb36c
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-29 10:20:10 +0800

    modified

--- poem_clean_then_start.sh
+++ poem_clean_then_start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./poem_end_train.sh && rm -rf logs/poems && rm -rf model/poems && ./poem_begin_train.sh
+./poem_end_train.sh && rm -rf logs/poems && rm -rf logs/poems.out && rm -rf model/poems && ./poem_begin_train.sh
 
 
--- shakespeare_clean_then_start.sh
+++ shakespeare_clean_then_start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./shakespeare_end_train.sh && rm -rf logs/shakespeare && rm -rf model/shakespeare && ./shakespeare_begin_train.sh
+./shakespeare_end_train.sh && rm -rf logs/shakespeare && rm -rf logs/shakespeare.out && rm -rf model/shakespeare && ./shakespeare_begin_train.sh
 
 
